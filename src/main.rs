054ff81812b4f7040383ab53c31a107cc8a7a333
Giovanni Cherubin <g.chers@gmail.com>
2018-12-15 11:20:51 +0000

    Add doc to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
+//! F-BLEAU is a tool for estimating the leakage of a system about its secrets
+//! in a black-box manner (i.e., by only looking at examples of secret inputs
+//! and respective outputs). It considers a generic system as a black-box,
+//! taking secret inputs and returning outputs accordingly, and it measures
+//! how much the outputs "leak" about the inputs.
+//!
+//! F-BLEAU is based on the equivalence between estimating the error of a
+//! Machine Learning model of a specific class and the estimation of
+//! information leakage [1,2,3].
+//!
+//! This code was also used for the experiments of [2] on the following
+//! evaluations: Gowalla, e-passport, and side channel attack to finite field
+//! exponentiation.
+//!
+//! # Getting started
+//!
+//! F-BLEAU takes as input CSV data containing examples of system's inputs
+//! and outputs.
+//! It currently requires two CSV files as input: a _training_ file and a
+//! _validation_ (or _test_) file, such as:
+//!
+//!     0, 0.1, 2.43, 1.1
+//!     1, 0.0, 1.22, 1.1
+//!     1, 1.0, 1.02, 0.1
+//!     ...
+//!
+//! where the first column specifies the secret, and the remaining ones
+//! indicate the output vector.
+//!
+//! It runs a chosen method for estimating the Bayes risk (smallest probability
+//! of error of an adversary at predicting a secret given the respective output),
+//! and relative security measures.
+//!
+//! The general syntax is:
+//!
+//!     fbleau <estimate> [options] <train> <test>
+//!
+//! ## Estimates
+//!
+//! Currently available estimates:
+//!
+//! **log** k-NN estimate, with `k = ln(n)`, where `n` is the number of training
+//! examples.
+//!
+//! **log 10** k-NN estimate, with `k = log10(n)`, where `n` is the number of
+//! training examples.
+//!
+//! **frequentist** (or "lookup table") Standard estimate. Note that this
+//! is only applicable when the outputs are finite; also, it does not scale
+//! well to large systems (e.g., large input/output spaces).
+//!
+//! Bounds and other estimates:
+//!
+//! **nn-bound** Produces a lower bound of R* discovered by Cover and Hard ('67),
+//! which is based on the error of the NN classifier (1-NN).
+//!
+//! **--knn** Runs the k-NN classifier for a fixed k to be specified.
+//! Note that this _does not_ guarantee convergence to the Bayes risk.
+//!
+//! ## Further options
+//!
+//! By default, `fbleau` runs until a convergence criterion is met.
+//! We usually declare convergence if an estimate did not vary more
+//! than `--delta`, either in relative (default) or absolute (`--abs`) value,
+//! from its value in the last `q` examples (where `q` is specified with
+//! `--qstop`).
+//! One can specify more than one deltas as comma-separated values, e.g.:
+//! `--delta=0.1,0.01,0.001`.
+//!
+//! Optionally, one may choose to let the estimator run for all the training
+//! set (`--run-all`), in which case `fbleau` will still report how many
+//! examples where required for convergence.
+//!
+//! When the system's outputs are vectors, `fbleau` by default scales their
+//! values. The option `--no-scale` prevents this (not recommended in
+//! general).
 extern crate ndarray;
 extern crate docopt;
 #[macro_use]
@@ -118,6 +194,8 @@ fn k_from_n(args: &Args) -> Box<Fn(usize) -> usize> {
     }
 }
 
+/// Prints several security measures that can be derived from a Bayes risk
+/// estimate and Random guessing error.
 fn print_all_measures(bayes_risk_estimate: f64, random_guessing: f64) {
     println!("Multiplicative Leakage: {}",
              multiplicative_leakage(bayes_risk_estimate, random_guessing));
